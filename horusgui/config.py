37f55aed23c043dc21cc484cdc461b3add784d84
Mark Jessop <darkside@Marks-MacBook-Pro.local>
2022-03-06 15:58:39 +1030

    Tweak valid versions to load config from

--- horusgui/config.py
+++ horusgui/config.py
@@ -72,7 +72,7 @@ def read_config(widgets):
     """ Read in configuration settings from Qt """
     global qt_settings, default_config
 
-    OK_VERSIONS = [__version__, '0.3.1', '0.2.1']
+    OK_VERSIONS = [__version__, '0.3.4', '0.3.1', '0.2.1']
     
     # Try and read in the version parameter from QSettings
     if qt_settings.value("version") not in OK_VERSIONS:
